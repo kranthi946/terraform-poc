e1eca9545772d8e29b1675ac5019099150f43c1d
kranthi946 <kranthikumarkattatech@gmail.com>
2024-11-27 22:33:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("credentials.json")}"
+#credentials = "${file("credentials.json")}"
 project = var.project_id
 region = var.vmregion
 }
